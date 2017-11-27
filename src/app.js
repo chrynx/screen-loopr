b923b369abd844ab98c5dbcef168838e5ad0654a
PullingStrings <drammatik@gmail.com>
2017-11-27 11:12:33 +0000

    buttons created

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Buttons from './components/Buttons.js';
 
 class App extends React.Component {
 
+
+  constructor(){
+    super();
+    this.state = { counter: 0 };
+  }
+
   render() {
+
     return (
-      <h1>WDI Project 4: MERN Stack App</h1>
+      <main>
+        <div className="container">
+          <h1>{this.state.counter}</h1>
+          <hr />
+          <Buttons />
+        </div>
+      </main>
+
+
     );
   }
 }
---
+++ src/components/Buttons.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Buttons = () => {
+  return (
+    <div>
+      <button className="btn btn-primary">button1</button>
+      {' '}
+      <button className="btn btn-primary">button2</button>
+      {' '}
+      <button className="btn btn-primary">button3</button>
+      {' '}
+      <button className="btn btn-primary">button4</button>
+    </div>
+  );
+};
+
+export default Buttons;
