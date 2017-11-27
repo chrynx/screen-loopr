0545415b17a679aecace92924a829ac59c4ae9f4
PullingStrings <drammatik@gmail.com>
2017-11-27 15:22:34 +0000

    javascript added

--- js/app.js
+++ js/app.js
@@ -1,14 +1,39 @@
 $(()=> {
+  const bpm = 500;
+  const $main = $('.main');
   const $buttons = $('button');
   const $firstPad = $('.firstPad');
   const $secondPad = $('.secondPad');
   const $thirdPad = $('.thirdPad');
   const $fourthPad = $('.fourthPad');
+  const $buttonPlay = $('.buttonPlay');
+
+  const $buttonLoop = $('.buttonLoop');
+  let loopActive = false;
+  const loopArr = [];
+  $buttonLoop.on('click', () => {
+    if(!loopActive) loopActive = true;
+    else loopActive = false;
+  });
+  // enter loop play button here
+  $buttonPlay.on('click', () => {
+    setInterval(() => {
+      let i = 0;
+      $main.append(loopActive[i]);
+      i = i + 1;
+      if(i === loopActive.length) clearInterval();
+    }, bpm);
+  });
+
   $buttons.on('click', (e) => {
     $(e.target).toggleClass('activeButton');
   });
+
   $firstPad.on('click', () => {
+    console.log(loopActive);
+    console.log(loopArr);
     console.log('clicked the first pad');
+    if(loopActive) loopArr.push('<audio autoplay><source src="sounds/clay.mp3"></audio>');
     $firstPad.html('<audio autoplay><source src="sounds/clay.mp3"></audio>');
   });
   $secondPad.on('click', () => {
